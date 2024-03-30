a772a179d693660e879fc6a34be2a48e45f4a432
Berkay <berkaykilicotu@gmail.com>
2024-03-30 22:51:29 +0300

    Menu created.

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,18 +1,61 @@
 import NetworkIssues.DNSError;
 import NetworkIssues.NetworkIssue;
+import NetworkIssues.Timestamp;
+
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        String domainName;
-        Scanner domainObj = new Scanner(System.in);
-        System.out.println("Enter the domain name you are trying to access: ");
-        domainName=domainObj.nextLine();
+        int menuChoice = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (menuChoice != 6) {
+            System.out.println("Please enter your network troubleshooting scenario:\n" +
+                    "1-DNS ERROR\n" +
+                    "2-DHCP ISSUE\n" +
+                    "3-ROUTING ERROR\n" +
+                    "4-NETWORK CONGESTION\n" +
+                    "5-COMBINATION SCENARIO\n" +
+                    "6-EXIT"
+            );
 
-        DNSError dnsError= new DNSError(domainName);
-        dnsError.printDomainInfo();
+            menuChoice = Integer.parseInt(scanner.nextLine());
 
+            switch (menuChoice) {
+                case 1:
+                    handleDNSIssue();
+                    break;
+                case 2:
 
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
     }
-}
+    public static Timestamp timestamp = new Timestamp();
+    private static void handleDNSIssue() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the domain name you are trying to access" + "For back to main menu press 2 ");
+            String userInput = scanner.nextLine();
+            if (userInput.equals("2")) {
+                break;
+            } else {
+                DNSError dnsError = new DNSError(userInput);
+                dnsError.printDomainInfo();
+                timestamp.getTimestamp();
+            }
+        }
+    }}
